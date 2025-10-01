11fa1605e01148f80e0ce742bc7fe87067c85ff5
Wisher7274 <16046697+wisher7274@user.noreply.gitee.com>
2025-10-02 01:23:24 +0800

    add main menu

---
+++ MainMenu.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void jump()
+	{
+		SceneTree st = this.GetTree();
+		st.ChangeSceneToFile("res://node_2d.tscn");
+	}
+}
